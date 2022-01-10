bc6580c3cdae53d31c6a84099b5ef7a700feb6ae
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-01-10 18:24:15 +0000

    Added Logic for adding item to Local Storage

--- books.js
+++ books.js
@@ -5,3 +5,41 @@ const bookList = document.querySelector('#book-list');
 
 let books = [];
 
+function clearInput(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+}
+
+const printErrorMsg = (message) => {
+  document.querySelector('.err-msg').innerHTML = message;
+  setTimeout(() => {
+    document.querySelector('.err-msg').innerHTML = '';
+  }, 1000);
+};
+
+function addBook(title, author){
+  const id = Date.now();
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  const bookObj = {
+                 id,
+                title,
+                author};
+   if (title === '' || author === ''){
+     printErrorMsg('Please Fill the Input Fields');
+   }else {
+     books.push(bookObj)
+     localStorage.setItem('books',JSON.stringify(books));
+     clearInput();
+   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  addBook(title, author);
+});
+});
