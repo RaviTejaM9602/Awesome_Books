ccc543e01b935f983ff772a7fd458a8165ca7ede
Ravi Teja <72485137+RaviTejaM9602@users.noreply.github.com>
2022-01-10 18:10:48 +0000

    Initialize JS id and class

--- books.js
+++ books.js
@@ -1,15 +1,7 @@
-let booksList = JSON.parse(localStorage.getItem('books') || '[]');
+const bookName = document.querySelector('#title');
+const authName = document.querySelector('#author');
+const addBtn = document.getElementById('add-btn');
 const bookList = document.querySelector('#book-list');
-const title = document.querySelector('#title');
-const authorName = document.querySelector('#author');
 
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
-}
-
-function addBook(book) {
-  books.push(book);
-  localStorage.setItem('books', JSON.stringify(books));
-}
+let books = [];
 
